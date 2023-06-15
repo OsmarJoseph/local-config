b7ac88d8e0858f224d5ada50d3ede74bd70e2af5
OsmarJoseph <osmarjoseph2013@hotmail.com>
2023-06-15 20:20:04 -0300

    add a global function to save and format files with prettier

--- nvim/lua/josean/plugins/lsp/null-ls.lua
+++ nvim/lua/josean/plugins/lsp/null-ls.lua
@@ -10,6 +10,12 @@ local formatting = null_ls.builtins.formatting -- to setup formatters
 -- to setup format on save
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+vim.format_with_prettier = function()
+  vim.api.nvim_command("write")
+  vim.cmd("!prettier --write %")
+  vim.cmd(":e %")
+end
+
 -- configure null_ls
 null_ls.setup({
   -- setup formatters & linters
