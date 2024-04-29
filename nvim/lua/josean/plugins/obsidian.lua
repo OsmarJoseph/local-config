b48d75a79d66b926811b5ecc734d709c72e7201d
OsmarJoseph <osmarjoseph2013@hotmail.com>
2024-04-29 15:30:00 -0300

    remove templates from obsidian config

--- nvim/lua/josean/plugins/obsidian.lua
+++ nvim/lua/josean/plugins/obsidian.lua
@@ -108,15 +108,6 @@ require("obsidian").setup(
       return out
     end,
 
-    -- Optional, for templates (see below).
-    templates = {
-      subdir = "templates",
-      date_format = "%Y-%m-%d",
-      time_format = "%H:%M",
-      -- A map for custom variables, the key should be the variable and the value a function
-      substitutions = {},
-    },
-
     -- Optional, customize the backlinks interface.
     backlinks = {
       -- The default height of the backlinks pane.
