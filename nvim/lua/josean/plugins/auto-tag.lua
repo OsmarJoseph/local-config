fe86edd2bf8ac769773ae1afbb8962620db78d89
OsmarJoseph <osmarjoseph2013@hotmail.com>
2024-08-16 19:23:16 -0300

    update auto-tag

--- nvim/lua/josean/plugins/auto-tag.lua
+++ nvim/lua/josean/plugins/auto-tag.lua
@@ -1,3 +1,8 @@
 require('nvim-ts-autotag').setup({
-  enable_close_on_slash = false,
+  opts = {
+    -- Defaults
+    enable_close = true,           -- Auto close tags
+    enable_rename = true,          -- Auto rename pairs of tags
+    enable_close_on_slash = false, -- Auto close on trailing </
+  }
 })
