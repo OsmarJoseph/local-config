66bc5d270cd5b06af116a7c41c0e3497553b65aa
OsmarJoseph <osmarjoseph2013@hotmail.com>
2024-12-16 19:44:23 -0300

    add daily template

--- nvim/.config/nvim/lua/josean/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/josean/plugins/obsidian.lua
@@ -77,7 +77,7 @@ require("obsidian").setup(
       -- Optional, if you want to change the date format of the default alias of daily notes.
       alias_format = "%B %-d, %Y",
       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = nil
+      template = "daily.md",
     },
     -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
     completion = {
